e50a7b61b5fd6e2e9a3d386e5afaa2ca6bee6ed2
xiufu.wang <407399291@qq.com>
2021-05-20 14:40:07 +0800

    优化： prod、test 、dev三个环境的构建

--- app-test.config.js
+++ app-test.config.js
@@ -1,28 +1,49 @@
+
+//PUBLIC_PATH: 该属性会影响所有的静态资源输入
 export default {
   /**
-   * 环境变量
+   * 环境变量 https://webpack.js.org/plugins/define-plugin/
    */
   envs: {
-    PUBLIC_PATH: '/wabbasetest'
+    PUBLIC_PATH: '/wabbasedev',
+    /**
+     * api/tools/http/request.js
+     */
+    VUE_APP_API_PATH: '/mock'
   },
   /**
-  * 主布局
-  */
+   * 主布局
+   */
   applayout: 'wukuang',
-  /**
-   * html标题
-   */
-  title: 'html标题',
-  /**
-   * js、asserts 路径前缀
-   */
-  publicPath: '',
 
-  // 代理
-  proxy: {
+  mode: 'dev',
 
-
-  },
-  mode: 'test'
-
+  /** 配置 */
+  devServer: {
+    port: 8080,
+    open: false,
+    proxy: {
+      '/appws': {
+        target: 'http://www.baidu.com/',
+        pathRewrite: { '^/appws': '' },
+        changeOrigin: true,
+        secure: true,
+        ws: true,
+        onProxyRes: function (proxyRes) {
+          const cookies = proxyRes.headers['set-cookie']
+          const cookieRegex = /Path=\/\/abs\//i
+          if (cookies) {
+            var newCookie = cookies.map(function (cookie) {
+              if (cookieRegex.test(cookie)) {
+                return cookie.replace(cookieRegex, 'Path=/')
+              }
+              return cookie
+            })
+            delete proxyRes.headers['set-cookie']
+            proxyRes.headers['set-cookie'] = newCookie
+          }
+        }
+      }
+    }
+  }
 }
